1c331f8cddb79853e27278843ab62576a794d143
PogorzelskiP <PogorzelskiP@outlook.com>
2022-07-15 15:48:09 +0200

    nowe repo

---
+++ Zmienne/DefiniowanieZmiennych.java
@@ -0,0 +1,11 @@
+package JavaStart.Zmienne;
+
+public class DefiniowanieZmiennych {
+    public static void main(String[] args) {
+        float a = 1.42f;
+        float b = 2.12f;
+        float c = 4.21f;
+        System.out.printf("1.42 + 2.12 + 4.21 = %.2f \n",(1.42f + 2.12f + 4.21));
+        System.out.printf("%.2f + %.2f + %.2f = %.2f",a,b,c,(a+b+c));
+    }
+}
