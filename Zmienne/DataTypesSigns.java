83b6da231070559d91ed4183bdd50ffbe8745157
PogorzelskiP <PogorzelskiP@outlook.com>
2022-07-16 11:47:04 +0200

    DODANIE TRZEJ NOWYCH KLAS
    Dodałem trzy nowe zadania z ćwiczeń ze zmiennymi

---
+++ Zmienne/DataTypesSigns.java
@@ -0,0 +1,12 @@
+package JavaStart;
+
+public class DataTypesSigns {
+    public static void main(String[] args) {
+        char k = 0x004B;
+        char o = 0x004F;
+        char t = 0x0054;
+        System.out.print(k);
+        System.out.print(o);
+        System.out.print(t);
+    }
+}
